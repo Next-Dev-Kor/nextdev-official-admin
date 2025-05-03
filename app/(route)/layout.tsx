c95556f122030cdbb2aa91c6e56c4fd99ed4eb77
LeeBonHoon <80507816+LeeBonHoon1@users.noreply.github.com>
2025-05-03 21:21:35 +0900

    [ FEAT ] web admin db 동기화

--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -1,12 +1,7 @@
 import { PropsWithChildren } from "react";
 
-<<<<<<< HEAD
-const RootLayout = ({ children }: PropsWithChildren) => {
-  return <div>{children}</div>;
-=======
 const RootLayout = async ({ children }: PropsWithChildren) => {
   return <>{children}</>;
->>>>>>> d04d0301f0cdf74d44ca1bf91af868c8d6f8656d
 };
 
 export default RootLayout;
